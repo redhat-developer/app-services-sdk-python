f29dfb547d117f54ff54b1b8fc594e090efe4188
dimakis <dimitri.saridakis@gmail.com>
2022-08-23 14:26:52 +0100

    feat: addition of a kafka_mgmt example and docs

---
+++ examples/README.md
@@ -0,0 +1,21 @@
+## Prerequisites:
+
+- RHOAS CLI installed - it will be used for obtaining tokens for authentication.
+For more information about authentication please refer to SDK docs.
+- The user must be logged in to use the RHOAS CLI. This can be done useing the following command:
+
+```bash
+rhoas login
+```
+
+## Running examples
+
+To run examples execute the python modules using the newly aquired ACCESS_TOKEN as follows:
+
+### For kafkamgmt: 
+
+```bash
+ACCESS_TOKEN=`rhoas authtoken`python3 examples/kafka_mgmt/get_kafkas_example.py
+```
+This should return a list of kafkas.
+
---
+++ examples/kafka_mgmt/get_kafkas_example.py
@@ -0,0 +1,33 @@
+import dimakis_test_kafka_mgmt_sdk
+from pprint import pprint
+from dimakis_test_kafka_mgmt_sdk.api import default_api
+import os
+
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN'] 
+
+# Defining the host is optional and defaults to https://api.openshift.com
+# See configuration.py for a list of all supported configuration parameters.
+
+# The client must configure the authentication and authorization parameters
+# in accordance with the API server security policy.
+# Examples for each auth method are provided below, use the example that
+# satisfies your auth use case.
+configuration = dimakis_test_kafka_mgmt_sdk.Configuration(
+    host = "https://api.openshift.com",
+    access_token = ACCESS_TOKEN
+)
+
+
+
+# Enter a context with an instance of the API client
+with dimakis_test_kafka_mgmt_sdk.ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+    api_instance = default_api.DefaultApi(api_client)
+
+    try:
+        api_response = api_instance.get_kafkas()
+        pprint(api_response)
+    except dimakis_test_kafka_mgmt_sdk.ApiException as e:
+        print("Exception when calling DefaultApi->create_kafka: %s\n" % e)
+    except Exception as excep:
+        print(f'Exception {excep:}')
