f8aa4aa8bdc6ddb6aafdd4ce4db14e4f0084eb82
dimakis <dimitri.saridakis@gmail.com>
2022-08-26 10:51:41 +0100

    chore: update generate errors script

--- .errors/index.js
+++ .errors/index.js
@@ -1,19 +1,19 @@
 module.exports ={
     kafka: {
         definition: require("./errors_kafka_mgmt.json"),
-        file: "sdks/kafka_management_sdk/apiv1/errors.ts"
+        file: "sdks/kafka_mgmt_sdk/errors.py"
     },
     srs:  {
         definition: require("./errors_srs_mgmt.json"),
-        file: "sdks/registry_management_sdk/apiv1/errors.ts"
+        file: "sdks/registry_mgmt_sdk/errors.py"
     },
-    // connector: {
-    //     definition: require("./errors_connector_mgmt.json"),
-    //     file: "connector_management_sdk/apiv1/errors.ts"
-    // }, 
-    // kafkainstance: {
-    //     definition: require("./errors_kafka_instance.json"),
-    //     file: "kafka_instance_sdk/apiv1/errors.ts"
-    // }, 
+    connector: {
+        definition: require("./errors_connector_mgmt.json"),
+        file: "sdks/connector_mgmt_sdk/errors.py"
+    }, 
+    kafkainstance: {
+        definition: require("./errors_kafka_instance.json"),
+        file: "sdks/kafka_instance_sdk/errors.py"
+    }, 
 }
 
