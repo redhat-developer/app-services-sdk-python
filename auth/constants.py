e2e3d2ee7b7aed518411a5eac6b0903e62c4ec66
dimakis <dimitri.saridakis@gmail.com>
2022-10-10 16:05:06 +0100

    refactor: refactor auth and add constants

---
+++ auth/constants.py
@@ -0,0 +1,3 @@
+DEFAULT_AUTH_URL  = "https://sso.redhat.com/auth/"
+DEFAULT_CLIENT_ID = "cloud-services"
+REALM_NAME = "redhat-external"
--- auth/rhoas_auth.py
+++ auth/rhoas_auth.py
@@ -1,17 +1,21 @@
 from keycloak import KeycloakOpenID
-from os import environ;
+from os import environ
 
-def get_access_token():
-    offlineToken = environ.get('OFFLINE_TOKEN')
-    # Configure client
-    keycloak_openid = KeycloakOpenID(server_url="https://sso.redhat.com/auth/",
-                                 client_id="cloud-services",
-                                 realm_name="redhat-external")
+from auth.constants import DEFAULT_AUTH_URL, DEFAULT_CLIENT_ID, REALM_NAME;
+
+def get_access_token(offline_token):
+    try:
+        if offline_token is None:
+            offline_token = environ.get('OFFLINE_TOKEN')
+    except KeyError:
+        print("Offline token not found in environment variables")
+        return None
+    
+    # Configure keycloak_openid client 
+    keycloak_openid = KeycloakOpenID(server_url=DEFAULT_AUTH_URL,
+                                 client_id=DEFAULT_CLIENT_ID,
+                                 realm_name=REALM_NAME,
+                                 )
     # Refresh token
-    token = keycloak_openid.refresh_token(offlineToken)
+    token = keycloak_openid.refresh_token(offline_token)
     return token
-
-
-
-
- 
